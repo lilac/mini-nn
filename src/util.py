91829e374ef9390527bbc9b77228d95741947410
Junjun Deng <cnjamesdeng@gmail.com>
2017-07-21 18:36:13 +0800

    Support predicting digit from images

--- src/util.py
+++ src/util.py
@@ -33,7 +33,33 @@ def evaluate(args):
     f = gzip.open(args.model, "rb")
     net = cPickle.load(f)
     print "Network layers: {}".format(net.sizes)
-    evaluate_data(net, test_set)
+    if args.dir:
+        infer(net, args.dir)
+    else:
+        evaluate_data(net, test_set)
+
+
+def rgb2gray(rgb):
+    import numpy as np
+
+    return np.dot(rgb[..., :3], [0.299, 0.587, 0.114])
+
+
+def infer(net, fpath):
+    from scipy import misc, ndimage
+    import numpy as np
+    import glob
+
+    for image_path in glob.glob(fpath + "/*/*.png"):
+        # image = ndimage.imread(image_path, mode='RGB')
+        # image = rgb2gray(image)
+        image = ndimage.imread(image_path, flatten=True)
+        img = misc.imresize(image, (28, 28))
+        # image.resize((28, 28, 1))
+        x = img.reshape((784, 1)).astype(np.float32)
+        x *= 1.0 / 255
+        res = net.infer(x)
+        print "{}: infer as {}".format(image_path, res)
 
 
 def evaluate_data(net, data):
@@ -70,6 +96,7 @@ def main():
     trainParser.set_defaults(func=train)
 
     evalParser.add_argument("model", help="model file")
+    evalParser.add_argument('-d', "--dir", help="test images directory")
     evalParser.set_defaults(func=evaluate)
 
     args = parser.parse_args()
